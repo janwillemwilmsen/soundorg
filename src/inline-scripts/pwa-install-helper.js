4e4a8b32d3a39be0c55c1095242ac8a1ddbf03d3
Jan Willem Wilmsen <janwillemwilmsen@gmail.com>
2020-05-17 17:00:53 +0200

    remove scripts

--- src/inline-scripts/pwa-install-helper.js
+++ src/inline-scripts/pwa-install-helper.js
@@ -6,6 +6,6 @@ if (('customElements' in window) && ('BeforeInstallPromptEvent' in window)) {
   const button = document.getElementById('butPWAInstall');
   button.addEventListener('pwa-install', (e) => {
     const d = e.detail;
-    gaEvent('PWAInstall', d.action, d.label, d.value, d.nonInteraction);
+   
   });
 }
